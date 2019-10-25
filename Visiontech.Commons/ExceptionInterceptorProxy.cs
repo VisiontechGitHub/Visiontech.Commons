9cbb7b945b303193398629a48e55f21d41a22dfb
Alessandro Moscatelli <alessandro.moscatelli@visiontech.cloud>
2019-10-25 02:50:18 +0200

    dynamic proxy

---
+++ Visiontech.Commons/ExceptionInterceptorProxy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SoapClientService
+{
+    public class ExceptionInterceptorProxy<T> : DispatchProxy
+    {
+
+        private T proxified;
+        private ICollection<Action<Exception>> handlers;
+
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            try {
+                return targetMethod.Invoke(proxified, args);
+            } catch (Exception exception)
+            {
+                foreach (Action<Exception> handler in handlers)
+                {
+                    handler.Invoke(exception);
+                }
+                return default;
+            }
+        }
+
+        public static T Create(T proxified, ICollection<Action<Exception>> handlers) {
+            T proxy = Create<T, ExceptionInterceptorProxy<T>>();
+            (proxy as ExceptionInterceptorProxy<T>).proxified = proxified;
+            (proxy as ExceptionInterceptorProxy<T>).handlers = handlers;
+            return proxy;
+        }
+
+    }
+}
---
+++ Visiontech.Commons/HttpWebResponseInterceptorProxy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+
+namespace SoapClientService
+{
+    public class HttpWebResponseInterceptorProxy<T> : ExceptionInterceptorProxy<T>
+    {
+
+        public static T Create(T proxified, ICollection<Action<HttpWebResponse>> handlers) {
+            return Create(proxified, new Collection<Action<Exception>> {
+                exception => 
+                {
+                    for (Exception e = exception; e is object; e = e.InnerException)
+                        if (e is WebException webException && webException.Response is HttpWebResponse httpWebResponse)
+                            foreach (Action<HttpWebResponse> handler in handlers)
+                                handler.Invoke(httpWebResponse);
+                } 
+            }
+            );
+        }
+
+    }
+}
